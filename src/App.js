d20263cf3b1db5958eac412915e35e1b2865db8d
Mainuddin Sani <main12sani@gmail.com>
2022-10-17 22:53:46 +0600

    tailwind first  project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="bg-black p-3">
+        <p className="text-blue-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, iste perspiciatis. Neque sapiente eum expedita cumque repudiandae alias ab fugiat?</p>
+      </h1>
     </div>
   );
 }
