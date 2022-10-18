4c64b10a3698e2492f8aa467a0d46233310e5f7a
Mainuddin Sani <main12sani@gmail.com>
2022-10-18 23:37:48 +0600

    props, state learn & practice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
+import { useState } from "react";
 import Navbar from "./assets/components/Navbar";
 
 
 function App() {
+  const [count, setCount] = useState(0);
+  const increse = ()=>{
+    return setCount(count +1)
+  }
+  console.log(count);
+  const personArray = [
+    {
+      name: 'Zahid',
+      age: 18,
+      collage: 'Bangladesh University',
+      university: "University of Dhaka",
+    }, 
+    {
+      name: 'Mainuddin Sani',
+      age: 18,
+      collage: 'Bangladesh University',
+      university: "University of Dhaka",
+    }
+  ];
+
+
   return (
     <>
-      <Navbar></Navbar>
+      {
+        personArray.map((person, i) => <Person person={person} count={count} increse={increse} key={i}></Person>)
+      }
     </> 
   );
 }
 
+function Person(props){
+  const {name, collage, Number} = props.person
+  // const {name, proffesion} = props
+  return (
+    <div className="person">
+        <h1>{name}</h1>
+        <p>{collage}</p>
+
+        <p>Count : {props.count}</p>
+        <button className="px-3 py-2 rounded-md bg-yellow-500 text-fuchsia-100" onClick={props.increse}>plus</button>
+    </div>
+  )
+}
 export default App;
--- src/assets/components/Navbar.js
+++ src/assets/components/Navbar.js
@@ -2,15 +2,13 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-screen bg-slate-300 h-[80px] fixed drop-shadow-lg">
+    <div className="w-screen bg-slate-300 h-[80px] drop-shadow-lg">
       <div className="px-2 flex justify-between w-full h-full items-center">
         <div className="flex align-center">
           <h1 className="text-2xl font-bold sm:text-4xl">Sani</h1>
         </div>
         <ul>
-          <li>
-            <li>Home</li>
-          </li>
+          <li>Home</li>
         </ul>
       </div>
     </div>
